9036782ee3616f4acf9171a7a55ff38afc2b510a
Raimundo <raimundo.delamaza@mayor.cl>
2025-05-09 23:41:37 -0400

    fix:tokendatalogin[0]

--- LaCaseraFrontend/src/modules/auth/context/AuthGlobalState.jsx
+++ LaCaseraFrontend/src/modules/auth/context/AuthGlobalState.jsx
@@ -18,7 +18,7 @@ export const AuthProvider = ({children}) => {
         try {
             const dataLogin = await loginService({email, password});
 
-            const {token} = dataLogin[0];
+            const {token} = dataLogin;
             const user = dataLogin.data;
 
             if(!token || !user){
